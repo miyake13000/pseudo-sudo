a45fdf47813ee0405c72d2adfa7453b5f7642f78
miyake13000 <miyake2020@s.okayama-u.ac.jp>
2025-01-31 17:04:03 +0900

    Initial commit

---
+++ pseudo-sudo.sh
@@ -0,0 +1,25 @@
+function pseudo_sudo {
+        if [ $LANG = "ja_JP.UTF-8" ]; then
+            echo -n "[sudo] $USER のパスワード: "
+        else
+            echo -n "[sudo] password for $USER: "
+        fi
+        read -s $_hoge
+        echo
+        sleep 3
+        if [ $LANG = "ja_JP.UTF-8" ]; then
+            echo "すみません、もう一度試してください。"
+            echo -n "[sudo] $USER のパスワード: "
+        else
+            echo "sudo: sorry, try again."
+            echo -n "[sudo] password for $USER: "
+        fi
+        read -s $_hoge
+        echo
+        sleep 3
+        if [ $LANG = "ja_JP.UTF-8" ]; then
+            echo "sudo: パスワードが違います"
+        else
+            echo "sudo: incorrect password"
+        fi
+}
